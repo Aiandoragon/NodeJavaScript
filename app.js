bb1ab4d5ce9a860db85c7cf9d6881c4af6c46f87
Lenya <leonid.31.03.2000@yandex.ru>
2020-12-30 06:11:41 +0500

    Q3

--- app.js
+++ app.js
@@ -31,7 +31,24 @@ console.log(`Hello ${greeting2.name}`); //Hello Bob
 // greeting1.name тоже изменилось
 console.log(`Hello ${greeting1.name}`); //Hello Bob*/
 
-const welcome = require("./welcome");
+/*const welcome = require("./welcome");
  
 welcome.getMorningMessage();
-welcome.getEveningMessage();
+welcome.getEveningMessage();*/
+ 
+/*global.name = "Eugene";
+ 
+global.console.log(date);
+console.log(greeting.getMessage());*/
+
+/*let nodePath = process.argv[0];
+let appPath = process.argv[1];
+let name = process.argv[2];
+let age = process.argv[3];
+ 
+console.log("nodePath: " + nodePath);
+console.log("appPath: " + appPath);
+console.log();
+console.log("name: " + name);
+console.log("age: " + age);*/
+
--- greeting.js
+++ greeting.js
@@ -1,5 +1,5 @@
 //console.log("greeting module");
-let currentDate = new Date();
+/*let currentDate = new Date();
 module.exports.date = currentDate;
  
 module.exports.getMessage = function(name){
@@ -11,4 +11,17 @@ module.exports.getMessage = function(name){
     else
         return "Доброе утро, " + name;
 }
-module.exports.name = "Alice";
+module.exports.name = "Alice";*/
+let currentDate = new Date();
+ 
+global.date = currentDate;
+ 
+module.exports.getMessage = function(){
+    let hour = currentDate.getHours();
+    if(hour >16)
+        return "Добрый вечер, " + global.name;
+    else if(hour >10)
+        return "Добрый день, " + name;
+    else
+        return "Доброе утро, " + name;
+}
